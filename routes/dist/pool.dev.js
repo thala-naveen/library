133102ca5a2351fbcc889432ac1d9f5fbe0a7575
Naveen Mittal <20cs1076@mitsgwl.ac.in>
2023-04-01 17:08:54 +0530

    new pool

--- routes/dist/pool.dev.js
+++ routes/dist/pool.dev.js
@@ -3,11 +3,11 @@
 var mysql = require("mysql");
 
 var pool = mysql.createPool({
-  host: process.env.MYSQLHOST || "localhost",
-  port: process.env.MYSQLPORT || 3306,
+  host: process.env.MYSQLHOST || "containers-us-west-72.railway.app",
+  port: process.env.MYSQLPORT || 7610,
   user: process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "1234",
-  database: process.env.MYSQLDATABASE || "library",
+  password: process.env.MYSQLPASSWORD || "qSljKmIwoPXEFldDNvqV",
+  database: process.env.MYSQLDATABASE || "railway",
   connectionLimit: 100
 });
 module.exports = pool;
--- routes/pool.js
+++ routes/pool.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
-  host: process.env.MYSQLHOST || "localhost",
-  port:process.env.MYSQLPORT || 3306,
+  host: process.env.MYSQLHOST || "containers-us-west-72.railway.app",
+  port:process.env.MYSQLPORT || 7610,
   user:process.env.MYSQLUSER || "root",
-  password: process.env.MYSQLPASSWORD || "1234",
-  database: process.env.MYSQLDATABASE || "library",
+  password: process.env.MYSQLPASSWORD || "qSljKmIwoPXEFldDNvqV",
+  database: process.env.MYSQLDATABASE || "railway",
   connectionLimit: 100,
 });
 
