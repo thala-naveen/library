e81e9fb1172defb2be6463c6392ee76077c74e80
Naveen Mittal <20cs1076@mitsgwl.ac.in>
2023-04-01 16:58:04 +0530

    host again

--- routes/pool.js
+++ routes/pool.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  port:process.env.DB_PORT|| 3306,
-  user:process.env.DB_USER|| "root",
+  port:process.env.DB_PORT || 3306,
+  user:process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "1234",
   database: process.env.DB_NAME || "library",
   connectionLimit: 100,
