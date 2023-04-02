82804760fd382797794d376ba9c28afc200ab931
Naveen Mittal <20cs1076@mitsgwl.ac.in>
2023-04-02 21:12:27 +0530

    credentials

--- auth.js
+++ auth.js
@@ -3,9 +3,9 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 
 passport.use(new GoogleStrategy({
-  clientID:'444553006193-54t7ik3ukce5c8ieoq106hrto3s11c3c.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: "http://localhost:4241/auth/google/callback",
+  clientID:'444553006193-ne17qt4h6roc0o594g7jctdrc5rofpoq.apps.googleusercontent.com',
+  clientSecret: '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',
+  callbackURL: "https://library-production-0c1b.up.railway.app/auth/google/callback",
   passReqToCallback: true,
 },
 function(request, accessToken, refreshToken, profile, done) {
