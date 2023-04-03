6f27fcde7cdbae653a14cfc510e77716028b24fc
Naveen Mittal <20cs1076@mitsgwl.ac.in>
2023-04-03 22:57:51 +0530

    auth

--- dist/auth.dev.js
+++ dist/auth.dev.js
@@ -7,7 +7,7 @@ var GoogleStrategy = require('passport-google-oauth2').Strategy;
 passport.use(new GoogleStrategy({
   clientID: '444553006193-ne17qt4h6roc0o594g7jctdrc5rofpoq.apps.googleusercontent.com',
   clientSecret: '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',
-  callbackURL: "https://library-production-0c1b.up.railway.app/auth/google/callback",
+  callbackURL: "https://librarymits.me/auth/google/callback",
   passReqToCallback: true
 }, function (request, accessToken, refreshToken, profile, done) {
   return done(null, profile);
